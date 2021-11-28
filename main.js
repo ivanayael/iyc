84906e5aa7ed3a63bc77874b8333a5474a43aa46
Ivana Yael Currá <ivanayael@hotmail.com>
2021-11-28 17:37:11 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
 import { MeshPhongMaterial } from 'three';
 
 // Setup
